55df0c35ed1249ff19715e18f8dffe6d5c881421
Gordon Watts <gwatts@uw.edu>
2020-04-02 22:31:12 -0700

    Test to access property
    Make sure we have types set right of the index variables

--- tests/test_multiobject.py
+++ tests/test_multiobject.py
@@ -42,3 +42,16 @@ def test_object_compare(good_transform_request, reduce_wait_time, files_back_1):
         .Select("lambda e8: e8[0].Select(lambda e3: e8[1].Electrons().Select(lambda e7: e7.DeltaR(e3)))")
         .AsROOTTTree("file.root", "treeme", ['col1']))
     assert json['selection'] == txt
+
+
+def test_object_compare_eta(good_transform_request, reduce_wait_time, files_back_1):
+    df = xaod_table(f)
+    seq = df.jets.map(lambda j: df.Electrons.DeltaR(j.eta))
+    make_local(seq)
+    json = good_transform_request
+    txt = translate_linq(
+        f
+        .Select("lambda e1: (e1.jets(), e1)")
+        .Select("lambda e11: e11[0].Select(lambda e3: e11[1].Electrons().Select(lambda e10: e10.DeltaR(e3.eta())))")
+        .AsROOTTTree("file.root", "treeme", ['col1']))
+    assert json['selection'] == txt
