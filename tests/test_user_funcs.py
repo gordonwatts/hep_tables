881f646abd75333698cd916f9c64338104556a70
Gordon Watts <gwatts@uw.edu>
2020-04-06 16:12:46 -0700

    lambda and func in maps
    Just a test to make sure both features are working.

--- tests/test_user_funcs.py
+++ tests/test_user_funcs.py
@@ -20,13 +20,30 @@ def test_user_function_with_implied(good_transform_request, reduce_wait_time, fi
         make_local(seq)
 
 
-def test_user_function_with_map(good_transform_request, reduce_wait_time, files_back_1):
+def test_user_function_with_map_lambda(good_transform_request, reduce_wait_time, files_back_1):
     @user_func
     def tns(e1: float) -> float:
         assert False, 'this is a fake function and should never be called'
 
     df = xaod_table(f)
     seq = df.jets.pt.map(lambda j: tns(j))
+    make_local(seq)
+    json = good_transform_request
+    txt = translate_linq(f
+                         .Select("lambda e1: e1.jets()")
+                         .Select("lambda e4: e4.Select(lambda e2: e2.pt())")
+                         .Select("lambda e5: e5.Select(lambda e3: tns(e3))")
+                         .AsROOTTTree("file.root", "treeme", ['col1']))
+    assert clean_linq(json['selection']) == txt
+
+
+def test_user_function_with_map_func(good_transform_request, reduce_wait_time, files_back_1):
+    @user_func
+    def tns(e1: float) -> float:
+        assert False, 'this is a fake function and should never be called'
+
+    df = xaod_table(f)
+    seq = df.jets.pt.map(tns)
     make_local(seq)
     json = good_transform_request
     txt = translate_linq(f
