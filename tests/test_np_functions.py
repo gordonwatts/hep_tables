030256b8c4fc7c80dfe443b5ef68da3d22ec78de
Gordon Watts <gordon@gordonandpaula.net>
2020-07-22 23:53:35 -0400

    Fix up spelling and type errors
    Fixes #25

--- tests/test_np_functions.py
+++ tests/test_np_functions.py
@@ -9,15 +9,15 @@ from .conftest import extract_selection, translate_linq
 
 
 @pytest.mark.parametrize("apply_f, func_text", [
-    (lambda f1, f2: np.arctan2(f1, f2), 'atan2'),
-    (lambda f1, f2: np.ldexp(f1, f2), 'ldexp'),
-    (lambda f1, f2: np.power(f1, f2), 'pow'),
-    (lambda f1, f2: np.remainder(f1, f2), 'remainder'),
-    (lambda f1, f2: np.copysign(f1, f2), 'copysign'),
-    (lambda f1, f2: np.nextafter(f1, f2), 'nextafter'),
-    (lambda f1, f2: np.fmod(f1, f2), 'fmod'),
-    (lambda f1, f2: np.fmax(f1, f2), 'fmax'),
-    (lambda f1, f2: np.fmin(f1, f2), 'fmin'),
+    (lambda f1, f2: np.arctan2(f1, f2), 'atan2'),  # type: ignore
+    (lambda f1, f2: np.ldexp(f1, f2), 'ldexp'),  # type: ignore
+    (lambda f1, f2: np.power(f1, f2), 'pow'),  # type: ignore
+    (lambda f1, f2: np.remainder(f1, f2), 'remainder'),  # type: ignore
+    (lambda f1, f2: np.copysign(f1, f2), 'copysign'),  # type: ignore
+    (lambda f1, f2: np.nextafter(f1, f2), 'nextafter'),  # type: ignore
+    (lambda f1, f2: np.fmod(f1, f2), 'fmod'),  # type: ignore
+    (lambda f1, f2: np.fmax(f1, f2), 'fmax'),  # type: ignore
+    (lambda f1, f2: np.fmin(f1, f2), 'fmin'),  # type: ignore
 ])
 def test_numpy_functions_2arg(apply_f, func_text, servicex_ds):
     f = ServiceXDatasetSource(servicex_ds)
@@ -34,31 +34,31 @@ def test_numpy_functions_2arg(apply_f, func_text, servicex_ds):
 
 
 @pytest.mark.parametrize("apply_f, func_text", [
-    (lambda f: np.sin(f), 'sin'),
-    (lambda f: np.cos(f), 'cos'),
-    (lambda f: np.tan(f), 'tan'),
-    (lambda f: np.arcsin(f), 'asin'),
-    (lambda f: np.arccos(f), 'acos'),
-    (lambda f: np.arctan(f), 'atan'),
-    (lambda f: np.sinh(f), 'sinh'),
-    (lambda f: np.cosh(f), 'cosh'),
-    (lambda f: np.tanh(f), 'tanh'),
-    (lambda f: np.arcsinh(f), 'asinh'),
-    (lambda f: np.arccosh(f), 'acosh'),
-    (lambda f: np.arctanh(f), 'atanh'),
-    (lambda f: np.exp(f), 'exp'),
-    (lambda f: np.log(f), 'log'),
-    (lambda f: np.log10(f), 'log10'),
-    (lambda f: np.exp2(f), 'exp2'),
-    (lambda f: np.log1p(f), 'log1p'),
-    (lambda f: np.log2(f), 'log2'),
-    (lambda f: np.sqrt(f), 'sqrt'),
-    (lambda f: np.cbrt(f), 'cbrt'),
-    (lambda f: np.ceil(f), 'ceil'),
-    (lambda f: np.floor(f), 'floor'),
-    (lambda f: np.trunc(f), 'trunc'),
-    (lambda f: np.rint(f), 'rint'),
-    (lambda f: np.absolute(f), 'abs'),
+    (lambda f: np.sin(f), 'sin'),  # type: ignore
+    (lambda f: np.cos(f), 'cos'),  # type: ignore
+    (lambda f: np.tan(f), 'tan'),  # type: ignore
+    (lambda f: np.arcsin(f), 'asin'),  # type: ignore
+    (lambda f: np.arccos(f), 'acos'),  # type: ignore
+    (lambda f: np.arctan(f), 'atan'),  # type: ignore
+    (lambda f: np.sinh(f), 'sinh'),  # type: ignore
+    (lambda f: np.cosh(f), 'cosh'),  # type: ignore
+    (lambda f: np.tanh(f), 'tanh'),  # type: ignore
+    (lambda f: np.arcsinh(f), 'asinh'),  # type: ignore
+    (lambda f: np.arccosh(f), 'acosh'),  # type: ignore
+    (lambda f: np.arctanh(f), 'atanh'),  # type: ignore
+    (lambda f: np.exp(f), 'exp'),  # type: ignore
+    (lambda f: np.log(f), 'log'),  # type: ignore
+    (lambda f: np.log10(f), 'log10'),  # type: ignore
+    (lambda f: np.exp2(f), 'exp2'),  # type: ignore
+    (lambda f: np.log1p(f), 'log1p'),  # type: ignore
+    (lambda f: np.log2(f), 'log2'),  # type: ignore
+    (lambda f: np.sqrt(f), 'sqrt'),  # type: ignore
+    (lambda f: np.cbrt(f), 'cbrt'),  # type: ignore
+    (lambda f: np.ceil(f), 'ceil'),  # type: ignore
+    (lambda f: np.floor(f), 'floor'),  # type: ignore
+    (lambda f: np.trunc(f), 'trunc'),  # type: ignore
+    (lambda f: np.rint(f), 'rint'),  # type: ignore
+    (lambda f: np.absolute(f), 'abs'),  # type: ignore
     (lambda f: np.abs(f), 'abs'),
 ])
 def test_numpy_functions(apply_f, func_text, servicex_ds):
