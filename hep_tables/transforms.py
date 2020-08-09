2f0e55c37fbeba319c93e1918a95f4b178480ae8
Gordon Watts <gordon@gordonandpaula.net>
2020-08-09 15:16:41 -0400

    Simple transforms that can perform a Select
    Working on #33
    
    - API for a transform now has the fidelity to do the work
    - The single implementation shows the basics of what is needed as information.

--- hep_tables/transforms.py
+++ hep_tables/transforms.py
@@ -1,9 +1,18 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
 import ast
-from typing import List, Optional
+from hep_tables.utils import QueryVarTracker
+from typing import List, Dict, Optional
 
 from func_adl import EventDataset
 from func_adl.object_stream import ObjectStream
+from func_adl.util_ast import lambda_build
+
+
+class astIteratorPlaceholder(ast.AST):
+    '''A place holder for the actual variable that references
+    the main iterator sequence objects.
+    '''
+    pass
 
 
 class sequence_predicate_base(ABC):
@@ -12,20 +21,88 @@ class sequence_predicate_base(ABC):
     def __init__(self):
         pass
 
-    @abstractproperty
-    def args(self) -> List[ast.AST]:
-        raise NotImplementedError()
-
     @abstractmethod
-    def sequence(self, seq: Optional[ObjectStream]) -> ObjectStream:
+    def sequence(self, sequence: ObjectStream,
+                 seq_dict: Dict[ast.AST, ast.AST]) -> Dict[ast.AST, ast.AST]:
+        '''Apply the operation to the sequence `sequence`
+
+        Args:
+            sequence (ObjectStream): The main `func_adl` sequence that is being
+                                     that is being processed.
+
+            seq_dict (Dict[ast.AST, ast.AST]): What to use to refer to the sequence.
+            The targed of the dicts should contain `astIteratorPlaceholder` objects, which
+            are replaced by the proper reference object.
+            Mostly used when we are part of a tuple, for accessing parts of the tuple.
+
+        Raises:
+            NotImplementedError: [description]
+
+        Returns:
+            Dict[ast.AST, ast.AST]: [description]
+'''
         raise NotImplementedError()
 
 
 class sequence_transform(sequence_predicate_base):
     '''Takes a sequence of type L[T1] and translates it to L[T2].
     '''
-    def __init__(self):
-        pass
+    def __init__(self,
+                 dependent_asts: List[ast.AST],
+                 function: ast.AST):
+        '''Transforms a sequence with the lambda implied by the `function` argument.
+
+        TODO: Do we care about `dependent_asts`?
+
+        Args:
+            dependent_asts (List[ast.AST]): List of the `ast`'s that must be
+            replaced in the `function` `ast` during rendering by references to
+            the stream or monads.
+            function (ast.AST): The `ast` that should build the
+            `lambda` function. It contains a number of arguments that must
+            be replaced, as listed in the `dependent_asts` argument.
+
+        Notes:
+            The `function` will be modifed in place. When it is passed in, do
+            not use it for anything else or it might change underneath you!
+        '''
+        self._function = function
+        self._dependent_asts = dependent_asts
+
+        self._called = False
+
+    def sequence(self, sequence: ObjectStream,
+                 seq_dict: Dict[ast.AST, ast.AST],
+                 qt: QueryVarTracker) -> ObjectStream:
+        '''
+        Return a Select statement around the function we are given.
+        '''
+        # Replace the argument references
+        new_name = qt.new_var_name()
+        new_name_ast = ast.Name(id=new_name)
+
+        class replace_arg(ast.NodeTransformer):
+            def visit_astIteratorPlaceholder(self, node: astIteratorPlaceholder) -> ast.AST:
+                return new_name_ast
+
+        arg_replacements = {k: replace_arg().visit(v) for k, v in seq_dict.items()}
+
+        # Replace the arguments in the function
+        class replace_ast(ast.NodeTransformer):
+            def generic_visit(self, node: ast.AST) -> Optional[ast.AST]:
+                if node in arg_replacements:
+                    return arg_replacements[node]
+                return super().generic_visit(node)
+
+        assert not self._called, 'Internal error, due to ast replacement can be called once'
+        self._called = True  # Protect against assumption that will bite us miles from here.
+        replaced = replace_ast().visit(self._function)
+
+        # Build the lambda
+        lb = lambda_build(new_name, replaced)
+
+        # Return the call
+        return sequence.Select(lb)
 
 
 class root_sequence_transform(sequence_predicate_base):
@@ -42,9 +119,6 @@ class root_sequence_transform(sequence_predicate_base):
         '''
         return self._eds
 
-    def args(self) -> List[ast.AST]:
-        'There are no arguments for this sequence transform'
-        return []
-
-    def sequence(self, seq: Optional[ObjectStream]) -> ObjectStream:
+    def sequence(self, sequence: ObjectStream,
+                 seq_dict: Dict[ast.AST, ast.AST]) -> Dict[ast.AST, ast.AST]:
         raise NotImplementedError()
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,7 +1,10 @@
 import ast
+
+import pytest
+from hep_tables.utils import QueryVarTracker
 from typing import List, Optional
-from func_adl.object_stream import ObjectStream, ObjectStreamException
-from hep_tables.transforms import sequence_predicate_base
+from func_adl.object_stream import ObjectStream
+from hep_tables.transforms import astIteratorPlaceholder, sequence_predicate_base, sequence_transform
 
 
 def test_sequence_predicate_base():
@@ -16,3 +19,67 @@ def test_sequence_predicate_base():
             return []
 
     mtb()
+
+
+@pytest.fixture
+def mock_qt(mocker):
+    qt = mocker.MagicMock(spec=QueryVarTracker)
+    qt.new_var_name.return_value = 'e1000'
+    return qt
+
+
+def test_seq_trans_null():
+    sequence_transform([ast.Num(20)], ast.Num(20))
+
+
+def test_seq_trans_no_args(mock_qt):
+    s = sequence_transform([], ast.Num(20))
+    base_seq = ObjectStream(ast.Name(id='dude'))
+    new_seq = s.sequence(base_seq, {}, mock_qt)
+    assert ast.dump(new_seq._ast, annotate_fields=False) == "Call(Name('Select', Load()), [Name('dude'), Lambda(arguments([arg('e1000')]), Num(20))], [])"
+
+
+def test_seq_trans_one_args_no_repl(mock_qt):
+    a = ast.Num(10)
+    s = sequence_transform([a], ast.Num(20))
+    base_seq = ObjectStream(ast.Name(id='dude'))
+    new_seq = s.sequence(base_seq, {a: ast.Num(30)}, mock_qt)
+    assert ast.dump(new_seq._ast, annotate_fields=False) == "Call(Name('Select', Load()), [Name('dude'), Lambda(arguments([arg('e1000')]), Num(20))], [])"
+
+
+def test_seq_trans_one_args(mock_qt):
+    a = ast.Num(10)
+    s = sequence_transform([a], a)
+    base_seq = ObjectStream(ast.Name(id='dude'))
+    new_seq = s.sequence(base_seq, {a: ast.Num(30)}, mock_qt)
+    assert ast.dump(new_seq._ast, annotate_fields=False) == "Call(Name('Select', Load()), [Name('dude'), Lambda(arguments([arg('e1000')]), Num(30))], [])"
+
+
+def test_seq_trans_attr(mock_qt):
+    a_root: ast.AST = ast.Name(id='e')
+    add = sequence_transform([a_root], ast.Attribute(a_root, attr='Jets'))
+
+    base_seq = ObjectStream(ast.Name(id='dude'))
+
+    ref_var = astIteratorPlaceholder()
+    s = add.sequence(base_seq, {a_root: ref_var}, mock_qt)
+    assert ast.dump(s._ast, annotate_fields=False) == \
+        "Call(Name('Select', Load()), [Name('dude'), Lambda(arguments([arg('e1000')]), Attribute(Name('e1000'), 'Jets'))], [])"
+
+
+def test_seq_trans_two_arg(mock_qt):
+    a_root_1: ast.AST = ast.Name(id='arg1')
+    a_root_2: ast.AST = ast.Name(id='arg2')
+    add = sequence_transform([a_root_1, a_root_2],
+                             ast.BinOp(op=ast.Add(), left=a_root_1, right=a_root_2))
+
+    base_seq = ObjectStream(ast.Name(id='dude'))
+
+    ref_var = astIteratorPlaceholder()
+    s = add.sequence(base_seq, {
+        a_root_1: ast.Subscript(value=ref_var, slice=ast.Index(value=ast.Num(0))),
+        a_root_2: ast.Subscript(value=ref_var, slice=ast.Index(value=ast.Num(1)))
+    }, mock_qt)
+
+    assert ast.dump(s._ast, annotate_fields=False) == \
+        "Call(Name('Select', Load()), [Name('dude'), Lambda(arguments([arg('e1000')]), BinOp(Subscript(Name('e1000'), Index(Num(0))), Add(), Subscript(Name('e1000'), Index(Num(1)))))], [])"
